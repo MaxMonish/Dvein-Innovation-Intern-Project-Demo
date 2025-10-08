c5992763b5f9744b544a02f8ff5990caf1e5aa74
MaxMonish <monishbaskar01234@gmail.com>
2025-10-08 10:45:16 +0530

    task model created

---
+++ backend/src/controllers/taskController.js
@@ -0,0 +1,8 @@
+const Task=require("../models/Task");
+const User=require("../models/User");
+
+const createTask = async (req, res){
+    try{
+        const {title, }
+    }
+}
---
+++ backend/src/models/Task.js
@@ -0,0 +1,35 @@
+const mongoose=require("mongooose");
+
+const taskSchema=new mongoose.Schema(
+    {
+        title:{
+            type: String,
+            required: true
+        }, 
+        description:{
+            type: String,
+            required: true
+        },
+        assignedTo:{
+            type: mongoose.Schema.Types.ObjectID,
+            ref: "User",
+            required: true
+        },
+        createdBy:{
+            type: mongoose.Schema.Types.ObjectID,
+            ref: "User",
+            required: true
+        },
+        status:{
+            type: String,
+            enum: ["Assigned", "In Progess", "Pending", "Completed"],
+            default: "Assigned"
+        },
+        dueDate:{
+            type: Date
+        }
+    },
+    {timestamps: true}
+);
+
+module.exports=mongoose.model("Task", taskSchema);
